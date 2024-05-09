b6dc2e5ec83bbcd47af640af964824aa421e5462
Michael Lindsay <michaeldeanlindsay@gmail.com>
2024-05-09 14:01:16 -0700

    update example config file

--- config_example.py
+++ config_example.py
@@ -4,8 +4,13 @@ ct_private_key = ''
 st_private_key = ''
 wc_private_key = ''
 
-# Connection strings for storage; disregard.
-ct_connection_string = "DRIVER={ODBC Driver 17 for SQL Server};SERVER=PNASQL01;DATABASE=CT;UID=<input>;PWD=<input>;Trusted_Connection=YES"
-kp_connection_string = "DRIVER={ODBC Driver 17 for SQL Server};SERVER=PNASQL01;DATABASE=KP;UID=<input>;PWD=<input>;Trusted_Connection=YES"
-st_connection_string = "DRIVER={ODBC Driver 17 for SQL Server};SERVER=PNASQL01;DATABASE=ST;UID=<input>;PWD=<input>;Trusted_Connection=YES"
-wc_connection_string = "DRIVER={ODBC Driver 17 for SQL Server};SERVER=PNASQL01;DATABASE=WC;UID=<input>;PWD=<input>;Trusted_Connection=YES"
+# Local user.
+server = "PNASQL01"
+user = ""
+pwd = ""
+
+# Local connection strings.
+ct_connection_string = "DRIVER={ODBC Driver 17 for SQL Server};SERVER=PNASQL01;DATABASE=CT;UID="+user+";PWD="+pwd+";Trusted_Connection=YES"
+kp_connection_string = "DRIVER={ODBC Driver 17 for SQL Server};SERVER=PNASQL01;DATABASE=KP;UID="+user+";PWD="+pwd+";Trusted_Connection=YES"
+st_connection_string = "DRIVER={ODBC Driver 17 for SQL Server};SERVER=PNASQL01;DATABASE=ST;UID="+user+";PWD="+pwd+";Trusted_Connection=YES"
+wc_connection_string = "DRIVER={ODBC Driver 17 for SQL Server};SERVER=PNASQL01;DATABASE=WC;UID="+user+";PWD="+pwd+";Trusted_Connection=YES"
